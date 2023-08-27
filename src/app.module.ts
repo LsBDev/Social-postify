426bce9c20ea6e8bededd3506f589cf58412bbe9
LsB.Dev <prof.luifel@gmail.com>
2023-08-27 11:24:24 -0300

    feat: criação das entidades + implementação rota /health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, MediasModule, PublicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// neste local onde ficaram configurações iniciais da aplicação
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
